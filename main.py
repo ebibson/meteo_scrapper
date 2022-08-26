3db9a9d9a03aef3623a9d955aee67945ea35c0a7
Raphael Learn <ebibende1@gmail.com>
2022-08-26 16:12:56 +0100

    add a workflow

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import bs4
 from pprint import pprint
 
 
-def main():
-    site = requests.get("https://www.msn.com/fr-fr/meteo").text
-    soup = bs4.BeautifulSoup(site, 'html.parser')
+site = requests.get("https://www.msn.com/fr-fr/meteo").text
+soup = bs4.BeautifulSoup(site, 'html.parser')
 
-    someLinks = soup.find_all('a', class_="summaryTemperatureCompact-E1_1")
-    advice = soup.find_all('div', class_="summaryCaptionCompact-E1_1")
-    TOWN = soup.find_all('a', class_="location_name_main_container-E1_1")
-    summary = soup.find_all('p', class_="summaryDescCompact-E1_1")
-    date = soup.find_all('div', class_="labelUpdatetime-E1_1")
+advice = soup.find_all('div', class_="summaryCaptionCompact-E1_1")
+TOWN = soup.find_all('a', class_="location_name_main_container-E1_1")
+someLinks = soup.find_all('a', class_="summaryTemperatureCompact-E1_1")
+summary = soup.find_all('p', class_="summaryDescCompact-E1_1")
+date = soup.find_all('div', class_="labelUpdatetime-E1_1")
+
+def main():
+
     
     for dt in date:
         print(dt.text)
@@ -27,6 +29,13 @@ def main():
 
     for something in advice:
         print(something.text)
+
+def for_test():
+    global TOWN, summary, advice
+
+    assert TOWN is True
+    assert summary is True
+    assert advice is True
 
 if __name__ == "__main__":
     main()
